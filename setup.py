11099d36b571fff5d5e7bb53dcb345bb9a3f4fbf
Ishangarg082 <ishan.garg.082@gmail.com>
2024-07-21 11:55:54 +0530

    commiting project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Email_Classification",
+    version = "0.0.1",
+    author="Manav",
+    author_email="manavgarg026@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
